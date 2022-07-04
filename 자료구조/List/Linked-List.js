2637992161f0e25c58b832f4e7b77a2f870c1779
MJ <mj632900@gmail.com>
2022-07-05 01:51:20 +0900

    연결리스트 노드 추가

--- 자료구조/List/Linked-List.js
+++ 자료구조/List/Linked-List.js
@@ -20,4 +20,27 @@ LinkedList.prototype.size = function(){
 // isEmpty() : 객체 내 노드 존재 여부 확인
 LinkedList.prototype.isEmpty = function(){
     return this.length === 0;
-}
+};
+
+// printNode() : 노드 출력
+LinkedList.prototype.printNode = function(){
+    for (let node = this.head; node != null; node = node.next){
+        process.stdout.write(`${node.data} -> `);
+    }
+    console.log("null");
+};
+
+// append() : 연결리스트 가장 끝에 노드 추가
+LinkedList.prototype.append = function(value){
+    let node = new Node(value),
+        current= this.head;
+
+    if(this.head === null) {
+        this.head = node;
+    } else {
+        while(current.next !== null){
+            current = current.next;
+        }
+        current.next = node;
+    }
+};
