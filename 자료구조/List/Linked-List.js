4dab4bbf089fd78727edf132eb7086a023e108d9
MJ <mj632900@gmail.com>
2022-07-04 22:49:22 +0900

    연결 리스트 생성자 함수

---
+++ 자료구조/List/Linked-List.js
@@ -0,0 +1,23 @@
+// 각 노드가 데이터와 포인터를 가지며, 한 줄로 연결되어있는 방식으로 데이터를 저장하는 구조
+
+//Node() : data와 point를 가지고 있는 객체
+function Node(data){
+    this.data = data;
+    this.next = null;
+};
+
+//LinkedList() : head와 length를 가지고 있는 객체
+function LinkedList(){
+    this.head = null;
+    this.length = 0;
+};
+
+// size() : 연결 리스트 내 노드 개수 확인
+LinkedList.prototype.size = function(){
+    return this.length;
+};
+
+// isEmpty() : 객체 내 노드 존재 여부 확인
+LinkedList.prototype.isEmpty = function(){
+    return this.length === 0;
+}
