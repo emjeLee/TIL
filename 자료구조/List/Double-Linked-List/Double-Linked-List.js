d22feeaa300b3a7bee04f1df6173033e0ee41835
MJ <mj632900@gmail.com>
2022-07-12 23:33:27 +0900

    이중연결리스트 : printNode, printNodeInverse

--- 자료구조/List/Double-Linked-List/Double-Linked-List.js
+++ 자료구조/List/Double-Linked-List/Double-Linked-List.js
@@ -5,14 +5,14 @@ function Node(data){
     this.data = data;
     this.next = null;
     this.prev = null;
-}
+};
 
 //LinkedList() : head, tail과 length를 가지고 있는 객체
 function DoubleLinkedList(){
     this.head = null;
     this.tail = null;
     this.length = 0;
-}
+};
 
 //size() : 연결 리스트 내 노드 개수 확인
 DoubleLinkedList.prototype.size = function(){
@@ -23,3 +23,27 @@ DoubleLinkedList.prototype.size = function(){
 DoubleLinkedList.prototype.isEmpty = function(){
     return this.length === 0;
 };
+
+//pintNode() : 노드 정방향 출력
+DoubleLinkedList.prototype.printNode = function(){
+    process.stdout.write("head -> ");
+    for(let node = this.head; node != null; node = node.next){
+        process.stdout.write(`${node.data} - > `);
+    }
+    console.log("null");
+};
+
+// pintNodeInverse() : 노드 역방향 출력
+DoubleLinkedList.prototype.printNodeInverse = function(){
+    let temp = [];
+
+    process.stdout.write("null <- ");
+    for(let node = this.tail; node != null; node = node.prev){
+        temp.push(node.data);
+    }
+    for(let i = temp.length - 1; i >= 0; i--){
+        process.stdout.write(`${temp[i]} <- `);
+    }
+    console.log("tail");
+};
+
