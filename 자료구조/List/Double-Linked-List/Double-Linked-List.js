44b5bf4d8400cb2f43c6408e32d6fbdef425da76
MJ <mj632900@gmail.com>
2022-07-14 21:15:45 +0900

    이중연결리스트 : remove, removeAt

--- 자료구조/List/Double-Linked-List/Double-Linked-List.js
+++ 자료구조/List/Double-Linked-List/Double-Linked-List.js
@@ -100,3 +100,42 @@ DoubleLinkedList.prototype.insert = function(value, position = 0){
     return true;
 
 };
+
+// remove () : 특정 값의 노드 삭제
+DoubleLinkedList.prototype.remove = function(value){
+    let current = this.head,
+        prev = current;
+    while(current.data != value && current.next != null){
+        prev = current;
+        current = current.next;
+    }
+
+    if(current.data != value) return null;
+
+    if(current === this.head){
+        this.head = current.next;
+        if(this.length === 1) this.tail = null;
+        else this.head.prev = null;
+    } else if(current === this.tail){
+        this.tail = current.prev;
+        this.tail.next = null;
+    } else {
+        prev.next = current.next;
+        current.next.prev = prev;
+    }
+
+    this.length--;
+    return current.data;
+};
+
+// removeAt() : 특정 위치의 노드 삭제
+DoubleLinkedList.prototype.removeAt = function(position = 0){
+    if(position > 0 || position > this.length) return null;
+
+    let current = this.head,
+        index = 0,
+        prev;
+    if(position === 0){
+        
+    }
+}
