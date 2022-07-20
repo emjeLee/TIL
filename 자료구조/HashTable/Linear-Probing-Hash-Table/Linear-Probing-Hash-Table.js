94327b40563ab8ac8e7ace71aa6f6f7551f0cab9
MJ <mj632900@gmail.com>
2022-07-21 01:09:15 +0900

    선형해시테이블 : get, remove

--- 자료구조/HashTable/Linear-Probing-Hash-Table/Linear-Probing-Hash-Table.js
+++ 자료구조/HashTable/Linear-Probing-Hash-Table/Linear-Probing-Hash-Table.js
@@ -46,7 +46,7 @@ LinearHashTable.prototype.getBuffer = function(){
 
 // print() : 데이터 셋 출력
 LinearHashTable.prototype.print = function(){
-    for(let i=0; i < this.length; i++){
+    for(let i=0; i < this.table.length; i++){
         if(this.table[i]){
             console.log(`${i} -> ${this.table[i].key}: ${this.table[i].value}`)
         }
@@ -69,4 +69,37 @@ LinearHashTable.prototype.put = function(key, value){
     }   
     while (index !== startIndex);
     return false;
+};
+
+// get() : 데이터 조회
+LinearHashTable.prototype.get = function(key){
+    let index = this.hashCode(key);
+    let startIndex = index;
+
+    do{
+        if(this.table[index] !== undefined && this.table[index].key === key){
+            return true;
+        }
+        index = (index + 1) % HASH_SIZE;
+    } while(index !== startIndex);
+
+    return undefined;
+};
+
+// remove() : 데이터 삭제
+LinearHashTable.prototype.remove = function(key){
+    let index = this.hashCode(key);
+    let startIndex = index;
+
+    do{
+        if(this.table[index] !== undefined && this.table[index].key === key){
+            let element = this.table[index];
+            delete this.table[index];
+            this.length--;
+
+            return element;
+        }
+        index = (index + 1) % HASH_SIZE;
+    } while(index !== startIndex);
+    return undefined;
 };
