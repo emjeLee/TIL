4f4484b44d7e5625fdfd462131d15bc048ba63e1
MJ <mj632900@gmail.com>
2022-07-24 22:26:05 +0900

    데크 : 데이터 추가 / 삭제

--- 자료구조/Stack and Queue/Deque.js
+++ 자료구조/Stack and Queue/Deque.js
@@ -15,4 +15,22 @@ Deque.prototype.isEmpty = function(){
     return this.array.length === 0;
 };
 
+// pushFront() : 앞쪽 데이터 추가
+Deque.prototype.pushFront = function(element){
+    return this.array.unshift(element);
+};
 
+// popFront() : 앞쪽 데이터 삭제
+Deque.prototype.popFront = function(){
+    return this.array.shift();
+};
+
+// pushBack() : 뒤쪽 데이터 추가
+Deque.prototype.pushBack = function(element){
+    return this.array.push(element);
+};
+
+// popBack() : 뒤쪽 데이터 삭제
+Deque.prototype.popBack = function(){
+    return this.array.pop();
+};
