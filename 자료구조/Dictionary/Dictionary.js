7f83ee0cc354f8e12a8f13562df6cddfb426d74f
MJ <mj632900@gmail.com>
2022-07-26 18:37:10 +0900

    딕셔너리 : has, set, get, remove

--- 자료구조/Dictionary/Dictionary.js
+++ 자료구조/Dictionary/Dictionary.js
@@ -20,3 +20,26 @@ Dictionary.prototype.size = function(){
     return Object.keys(this.items).length;
 };
 
+// has() : 개체 존재 여부 확인 (key 정보를 배열로 반환)
+Dictionary.prototype.has = function(key){
+    return this.items.hasOwnProperty(key);
+};
+
+// set() : 개체 추가
+Dictionary.prototype.set = function(key, value){
+    return this.items[key] = value;
+};
+
+// get() : 개체의 value반환
+Dictionary.prototype.get = function(key){
+    return this.has(key) ? this.items[key] : undefined;
+}
+
+// remove() : 개체 삭제
+Dictionary.prototype.remove = function(key){
+    if(this.has(key)){
+        delete this.items[key];
+        return true;
+    }
+    return false;
+};
