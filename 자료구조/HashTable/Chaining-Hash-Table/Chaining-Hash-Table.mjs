aee6e21b3ba7eee8605c99b0c530c67ba6b44103
MJ <mj632900@gmail.com>
2022-07-28 00:52:04 +0900

    체이닝해시테이블

--- 자료구조/HashTable/Chaining-Hash-Table/Chaining-Hash-Table.mjs
+++ 자료구조/HashTable/Chaining-Hash-Table/Chaining-Hash-Table.mjs
@@ -36,3 +36,86 @@ ChainingHashTable.prototype.clear = function(){
 ChainingHashTable.prototype.size = function(){
     return this.length;
 };
+
+// put() : 데이터 추가
+ChainingHashTable.prototype.put = function(key, value){
+    let index = this.hashCode(key);
+    console.log(`key: ${key} -> index: ${index}`);
+    if(this.table[index] === undefined){
+        this.table[index] =  new LinkedList();
+    }
+
+    this.table[index].append(new Element(key, value));
+    this.length++;
+
+    return true;
+};
+
+// getBuffer() : 데이터 셋 반환
+ChainingHashTable.prototype.getBuffer = function(){
+    let array =[];
+    for (let i = 0; i < this.table.length; i++){
+        if(this.table[i]){
+            let current = this.table[i].head;
+            do {
+                array.push(current.data);
+                current = current.next;
+            } while(current);
+        }
+    }
+    return array;
+};
+
+// print() : 데이터 셋 출력
+ChainingHashTable.prototype.print = function(){
+    for(let i = 0; i < this.table.length; i++){
+        if(this.table[i]){
+            let current = this.table[i].head;
+            process.stdout.write(`#${i}`);
+            do {
+                process.stdout.write( `-> ${current.data.key}: ${current.data.value}`);
+                current = current.next;
+            } while(current);
+            console.log("");
+        }
+    }
+};
+
+// get() : 데이터 조회
+ChainingHashTable.prototype.get = function(key){
+    let index = this.hashCode(key);
+
+    if(this.table[index] !== undefined && !this.table[index].isEmpty()){
+        let current = this.table[index].head;
+        do {
+            if(current.data.key === key){
+                return current.data.value;
+            }
+            current = current.next;
+        } while(current);
+    }
+    return undefined;
+};
+
+// remove() : 데이터 삭제
+ChainingHashTable.prototype.remove = function(key){
+    let index = this.hashCode(key);
+    let element = undefined;
+
+    if(this.table[index] !== undefined){
+        let current = this.table[index].head;
+
+        do {
+            if(current.data.key === key){
+                element = current.data;
+                this.table[index].remove(current.data);
+                this.length--;
+                if(this.table[index].isEmpty()){
+                    delete this.table[index];
+                }
+            }
+            current = current.next;
+        } while(current);
+    } 
+    return element;
+};
