7136a53d85aa2268416fccfd3fe72564fd9bc89d
MJ <mj632900@gmail.com>
2022-07-27 22:50:34 +0900

    개체(entity) -> 객체 수정

--- 자료구조/Dictionary/Dictionary.js
+++ 자료구조/Dictionary/Dictionary.js
@@ -1,11 +1,11 @@
 // key-value 형태로 다양한 자료형 개체를 저장하는 자료구조(Map과 유사함)
 
-// Dictionary() : 개체를 저장 할 생성자
+// Dictionary() : 객체를 저장 할 생성자
 function Dictionary(items = {}){
     this.items = items;
 };
 
-// getBuffer() : 모든 개체 반환
+// getBuffer() : 모든 객체 반환
 Dictionary.prototype.getBuffer = function(){
     return {...this.items};
 };
@@ -20,22 +20,22 @@ Dictionary.prototype.size = function(){
     return Object.keys(this.items).length;
 };
 
-// has() : 개체 존재 여부 확인 (key 정보를 배열로 반환)
+// has() : 객체 존재 여부 확인 (key 정보를 배열로 반환)
 Dictionary.prototype.has = function(key){
     return this.items.hasOwnProperty(key);
 };
 
-// set() : 개체 추가
+// set() : 객체 추가
 Dictionary.prototype.set = function(key, value){
     return this.items[key] = value;
 };
 
-// get() : 개체의 value반환
+// get() : 객체의 value반환
 Dictionary.prototype.get = function(key){
     return this.has(key) ? this.items[key] : undefined;
 }
 
-// remove() : 개체 삭제
+// remove() : 객체 삭제
 Dictionary.prototype.remove = function(key){
     if(this.has(key)){
         delete this.items[key];
@@ -54,7 +54,7 @@ Dictionary.prototype.values = function(){
     return Object.values(this.items);
 };
 
-// each() : 모든 개체 요소에 대해 callback함수 수행 (:= foreach)
+// each() : 모든 객체 요소에 대해 callback함수 수행 (:= foreach)
 Dictionary.prototype.each = function(fn){
     for(let k in this.items){
         if(this.has(k)){
@@ -63,7 +63,7 @@ Dictionary.prototype.each = function(fn){
     }
 };
 
-// printDictionary() : 개체 출력 callback
+// printDictionary() : 객체 출력 callback
 function printDictionary(key, value){
     console.log(`key: ${key}`);
     console.log(`value: ${value}`);
