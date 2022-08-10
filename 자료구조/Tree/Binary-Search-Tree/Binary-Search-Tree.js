4ddacc3e5c253a218b181fa0f0bc417f0e0ed36c
MJ <mj632900@gmail.com>
2022-08-10 23:08:47 +0900

    이진 탐색 트리 : remove

--- 자료구조/Tree/Binary-Search-Tree/Binary-Search-Tree.js
+++ 자료구조/Tree/Binary-Search-Tree/Binary-Search-Tree.js
@@ -39,6 +39,7 @@ BinarySearchTree.prototype._inOrderTraverseNode = function(node, callback){
 // preOrderTraverseNode() : 중위 순회하며 노드 출력
 BinarySearchTree.prototype.inOrderTraverse = function(callback){
     this._inOrderTraverseNode(this.root, callback);
+    console.log("end")
 };
 
 // _minNode() : 반복문으로 트리를 순회하며 최솟값 노드 탐색
@@ -90,3 +91,68 @@ BinarySearchTree.prototype._searchNode = function(node, value){
 BinarySearchTree.prototype.search = function(value){
     return this._searchNode(this.root, value);
 };
+
+// _findMinNode() : 반복문으로 트리를 순회하며 최솟값을 보유한 노드 탐색
+BinarySearchTree.prototype._findMinNode = function(node){
+    while(node && node.left !== null){
+        node = node.left;
+    }
+    return node;
+};
+
+// _removeNode() : 재귀로 트리를 순회하며 값을 만족하는 노드를 찾고 삭제
+BinarySearchTree.prototype._removeNode =  function(node, value){
+    if(node === null) return null;
+
+    if(node.value === value){
+        // case 1 : leaf node
+        if (node.left === null && node.right === null) node = null;
+        // case 2 : 1 child node
+        else if(node.left === null){
+            node = node.right;
+        } else if(node.right === null){
+            node = node.left;
+        }
+        // case 3 : 2 childe node 
+        else {
+            let aux = this._findMinNode(node.right);
+            node.value = aux.value;
+            node.right = this._removeNode(node.right, aux.value);
+        }
+    } else if(node.value > value){
+        node.left = this._removeNode(node.left, value);
+    } else if(node.value < value){
+        node.right = this._removeNode(node.right, value);
+    }
+    return node;
+};
+
+// remove() : 노드 삭제
+BinarySearchTree.prototype.remove = function(value){
+    this.root = this._removeNode(this.root, value);
+};
+
+// make $ insert tree
+let tree = new BinarySearchTree();
+
+tree.insert("F");
+tree.insert("B");
+tree.insert("A");
+tree.insert("D");
+tree.insert("C");
+tree.insert("E");
+tree.insert("G");
+tree.insert("I");
+tree.insert("H");
+
+function printNode(node){
+    process.stdout.write(`${node.value} -> `);
+};
+
+tree.inOrderTraverse(printNode);
+tree.remove("A");
+tree.inOrderTraverse(printNode);
+tree.remove("B");
+tree.inOrderTraverse(printNode);
+tree.remove("G");
+tree.inOrderTraverse(printNode);
