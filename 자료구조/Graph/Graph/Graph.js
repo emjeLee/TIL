f4d31eacf5a931fff8164f8ff56c73d35120ef36
MJ <mj632900@gmail.com>
2022-08-13 23:24:00 +0900

    그래프 : 개수반환 및 출력

--- 자료구조/Graph/Graph/Graph.js
+++ 자료구조/Graph/Graph/Graph.js
@@ -38,9 +38,51 @@ Graph.prototype.removeVertex = function(v){
     }
 };
 
+// sizeVertex() : vertex 개수 반환
+Graph.prototype.sizeVertex = function(){
+    return Object.keys(this.edge).length;
+};
+
+// sizeEdge() : edge 개수 반환
+Graph.prototype.sizeEdge = function(vertex){
+    return this.edge[vertex] ? Object.keys(this.edge[vertex]).length : 0;
+};
+
+// print() : 현재 Graph 연결 상태 출력
+Graph.prototype.print = function(){
+    for(let vertex in this.edge){
+        let neighbors = this.edge[vertes];
+        if(neighbors.length === 0) continue;
+        process.stdout.write(`${vertex} -> `);
+        for(let j=0; j<neighbors.length; j++){
+            process.stdout.write(`${neighbors[j]}`);
+        }
+        console.log("");
+    }
+};
+
+// test
 let graph = new Graph();
 let vertices = ["A", "B", "C", "D", "E"]
 
 for(let i=0; i<vertices.length; i++){
     graph.addVertex(vertices[i]);
 };
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("A", "D");
+graph.addEdge("C", "G");
+graph.addEdge("D", "G");
+graph.addEdge("D", "H");
+graph.addEdge("B", "F");
+graph.addEdge("B", "I");
+graph.addEdge("E", "I");
+console.log(graph.edge);
+
+console.log(graph.sizeVertex());
+console.log(graph.sizeEdge("A"));
+console.log(graph.edge);
+graph.removeEdge("A", "B");
+console.log(graph.edge);
+console.log(graph.sizeEdge("A"));
