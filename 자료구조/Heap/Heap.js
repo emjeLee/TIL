dc8cad5813e1dd6c4a4e599c26eedac98719a018
MJ <mj632900@gmail.com>
2022-08-21 00:27:34 +0900

    힙 : 최대힙 추가

--- 자료구조/Heap/Heap.js
+++ 자료구조/Heap/Heap.js
@@ -80,35 +80,72 @@ Heap.prototype.bubbleDown = function(){
     let index = 0;
     while(this.leftChild(index) && (this.leftChild(index) < this.items[index] || this.rightChild(index) < this.items[index])){
         let childIndex = this.leftChildIndex(index);
-        console.log('1', childIndex)
-        console.log('left', this.leftChild(index))
-        console.log('right', this.rightChild(index))
         // if문이 실행되지 않는 오류발생. 확인해 보니 rightChild메서드에서 오타가 났다 () 대신 [] 로 오타가 났었음.
         if(this.rightChild(index) && this.rightChild(index) < this.items[childIndex]){
             childIndex = this.rightChildIndex(index);
-            console.log('2', childIndex)
         }
         this.swap(childIndex, index);
         index = childIndex;
     }
 };
 
+// 최대힙
+// insert2() : 신규 노드 추가
+Heap.prototype.insert2 = function(item){
+    this.items[this.size()] = item;
+    this.bubbleUp2();
+};
+
+// extract2() : root노드 반환 및 삭제
+Heap.prototype.extract2 = function(){
+    let item = this.items[0];
+    this.items[0] =  this.items[this.size() - 1];
+    this.items.pop();
+    this.bubbleDown2();
+    return item;
+};
+
+// bubbleUp2() : 추가 된 노드 위치 정렬
+Heap.prototype.bubbleUp2 = function(){
+    let index = this.size() - 1;
+    while(this.parent(index) && this.parent(index) < this.items[index]){
+        // changed from > to <
+        this.swap(this.parentIndex(index), index);
+        index = this.parentIndex(index);
+    }
+};
+
+// bubbleDown2() : 대체된 root 노드 위치 정렬
+Heap.prototype.bubbleDown2 = function(){
+    let index = 0;
+    while(this.leftChild(index) && (this.leftChild(index) > this.items[index] || this.rightChild(index) > this.items[index])){
+        let childIndex = this.leftChildIndex(index);
+        if(this.rightChild(index) && this.rightChild(index) > this.items[childIndex]){
+            childIndex = this.rightChildIndex(index);
+        }
+
+        this.swap(childIndex, index);
+        index = childIndex;
+    }
+}
+
+
 //test
-let minHeap = new Heap();
+let maxHeap = new Heap();
 
-minHeap.insert(90)
-minHeap.insert(15)
-minHeap.insert(10)
-minHeap.insert(7)
-minHeap.insert(12)
-minHeap.insert(2)
-minHeap.insert(8)
-minHeap.insert(3)
-console.log(minHeap)
+maxHeap.insert2(90)
+maxHeap.insert2(15)
+maxHeap.insert2(10)
+maxHeap.insert2(7)
+maxHeap.insert2(12)
+maxHeap.insert2(2)
+maxHeap.insert2(8)
+maxHeap.insert2(3)
+console.log(maxHeap)
 
-console.log(minHeap.extract());
-console.log(minHeap)
-console.log(minHeap.extract());
-console.log(minHeap)
-console.log(minHeap.extract());
-console.log(minHeap)
+console.log(maxHeap.extract2());
+console.log(maxHeap.extract2());
+console.log(maxHeap.extract2());
+console.log(maxHeap.extract2());
+console.log(maxHeap.extract2());
+console.log(maxHeap.extract2());
